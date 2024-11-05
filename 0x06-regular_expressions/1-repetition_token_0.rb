ba49630b355d7cd2baef0376273763995b4eb158
AbdelrahmanElZahed <abdelrahmanelzahed94@gmail.com>
2024-11-05 14:45:58 +0200

    0x06-regular_expressions

--- 0x06-regular_expressions/1-repetition_token_0.rb
+++ 0x06-regular_expressions/1-repetition_token_0.rb
@@ -1,14 +1,14 @@
 #!/usr/bin/env ruby
-# This script checks if the given string matches the regex pattern
+# This script checks if the given string matches the specific regex pattern
+# Matches only strings with "hbttn", "hbtttn", "hbttttn", and "hbtttttn"
 
-# Regular expression pattern
+# Regular expression pattern to match only 2 to 4 occurrences of 't'
 pattern = /^hbtt{2,4}n$/
 
-# Accepts one argument
+# Accepts one argument from the command line
 input = ARGV[0]
 
-# Perform the match
+# Perform the match and print the input if it matches the pattern
 if input.match?(pattern)
   puts input
 end
-
