9e4865a6302c81737a20311cdc35af2827fe09ed
AbdelrahmanElZahed <abdelrahmanelzahed94@gmail.com>
2025-03-07 05:10:47 +0200

    1-user_limit.pp

--- 0x1B-web_stack_debugging_4/0-the_sky_is_the_limit_not.pp
+++ 0x1B-web_stack_debugging_4/0-the_sky_is_the_limit_not.pp
@@ -1,21 +1,6 @@
-exec { 'fix-nginx-high-concurrency':
-  command => "/bin/sed -i 's/worker_connections.*/worker_connections 4096;/' /etc/nginx/nginx.conf && systemctl restart nginx",
-  onlyif  => "grep -q 'worker_connections 1024;' /etc/nginx/nginx.conf",
-}
+# fix nginx to accept and serve more requests
 
-exec { 'increase-ulimit':
-  command => "/bin/echo 'ulimit -n 65536' >> /etc/security/limits.conf && ulimit -n 65536",
-  unless  => "ulimit -n | grep -q '65536'",
+exec {'modify max open files limit setting':
+  command => 'sed -i "s/15/4096/" /etc/default/nginx && sudo service nginx restart',
+  path    => '/usr/local/sbin:/usr/local/bin:/usr/sbin:/usr/bin:/sbin:/bin:/usr/games:/usr/local/games',
 }
-
-exec { 'tune-kernel-parameters':
-  command => "/sbin/sysctl -w net.core.somaxconn=65535 && /sbin/sysctl -w net.ipv4.tcp_max_syn_backlog=65535 && /sbin/sysctl -w net.ipv4.ip_local_port_range='1024 65000'",
-  unless  => "/sbin/sysctl net.core.somaxconn | grep -q '65535'",
-}
-
-exec { 'reload-sysctl':
-  command => "/sbin/sysctl -p",
-  refreshonly => true,
-  subscribe => Exec['tune-kernel-parameters'],
-}
-
